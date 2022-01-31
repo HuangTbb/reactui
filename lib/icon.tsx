9a584f6b6c1909b3048f6e688ed42b6a9b687521
hjy <5336772+huangtobe@user.noreply.gitee.com>
2022-01-31 22:51:49 +0800

    添加svg  onClick参数

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -2,11 +2,12 @@ import React from "react"
 import './importIcons'
 import './icon.scss'
 interface IconProps {
-  name: string
+  name: string;
+  onClick?: React.MouseEventHandler<SVGElement>
 }
 const Icon: React.FunctionComponent<IconProps>=(props)=>{
   return (
-    <svg className="boxui-icon">
+    <svg className="boxui-icon" onClick={props.onClick}>
       <use xlinkHref={`#${props.name}`} />
     </svg>
 
--- lib/index.tsx
+++ lib/index.tsx
@@ -1,9 +1,11 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import Icon from './icon';
-
+const fn: React.MouseEventHandler = (e)=>{
+    console.log(e.target)
+}
 ReactDOM.render(<div>  
     <Icon name="wechat"/>
     <Icon name="alipay"/>
-    <Icon name="qq"/>
+    <Icon name="qq" onClick={fn}/>
     </div>, document.querySelector('#root'))
